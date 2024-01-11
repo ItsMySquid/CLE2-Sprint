46a755b4cf10d2ed87cf2e8879e45f50cf3cd5fd
D_Zenith <dlw2002@outlook.com>
2024-01-11 15:29:57 +0100

    Begin aan front end en nav bar

--- Website/index.php
+++ Website/index.php
@@ -4,10 +4,20 @@
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
         <link rel='stylesheet' href="style/style.css">
-        <title>Welcome to Elfie Creates.</title>
+        <title>Welcome to Elfie Creates</title>
     </head>
 
     <body>
+        <nav>
+            <img class="nav_logo" src="images/logo.png" alt="">
+            <div class="links_nav">
+                <a href="">Afspraak Maken</a>
+                <a href="">Producten</a>
+                <a href="">Over Ons</a>
+            </div>
+            <img class="nav_logo" src="images/logo.png" alt="">
+        </nav>
+
         <h1>This is a Heading</h1>
         <p>This is a paragraph.</p>
     </body>
