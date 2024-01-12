c67ed9927806d072646b4dc94dc2e80a53d71252
D_Zenith <dlw2002@outlook.com>
2024-01-12 11:24:46 +0100

    Alle pagina's gemaakt, links werken. Met een paar uitzonderingen.

--- Website/includes/over_ons.php
+++ Website/includes/over_ons.php
@@ -3,17 +3,17 @@
         <meta charset='utf-8'>
         <meta http-equiv='X-UA-Compatible' content='IE=edge'>
         <meta name='viewport' content='width=device-width, initial-scale=1'>
-        <link rel='stylesheet' href="style/style.css">
-        <title>Welcome to Elfie Creates</title>
+        <link rel='stylesheet' href="../style/style.css">
+        <title>Over Elfie Creates</title>
     </head>
 
     <body>
         <nav>
-            <img class="nav_logo" src="images/logo.png" alt="">
+            <img class="nav_logo" src="../images/logo.png" alt="">
             <div class="links_nav">
-                <a href="includes/afspraak.php">Afspraak Maken</a>
-                <a href="includes/producten.php">Diensten</a>
-                <a href="includes/over_ons.php">Over Ons</a>
+                <a href="../includes/afspraak.php">Afspraak Maken</a>
+                <a href="../includes/producten.php">Diensten</a>
+                <a href="../includes/over_ons.php">Over Ons</a>
             </div>
         </nav>
 
