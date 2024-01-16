e28b2275704c31a10a15837d354b5a315eaa4e7a
D_Zenith <dlw2002@outlook.com>
2024-01-16 14:18:39 +0100

    More pages done.

--- Website/over_ons.php
+++ Website/over_ons.php
@@ -17,7 +17,21 @@
             </div>
         </nav>
 
-        <h1>This is a Heading</h1>
-        <p>This is a paragraph.</p>
+        <h1>Wie ben ik?</h1>
+        <p>
+            Hallo, ik ben Elif, een 40-jarige moeder van vier prachtige kinderen, waarvan twee te maken hebben met ADHD en dyslexie. Plannen was voor ons geen routine, maar eerder een uitdaging die we moesten overwinnen.
+            Mijn reis begon toen ik mijn eerste planner maakte voor mijn zoon, in een poging om structuur en orde te brengen in zijn dagelijkse leven. Na verschillende iteraties en intensieve tests ontdekte ik dat deze aangepaste planners niet alleen hem, maar ook ons als gezin enorm hielpen.
+            Wat begon als een persoonlijke missie om mijn kinderen te ondersteunen, groeide al snel uit tot een diepere passie. Ik besloot mijn ervaring te delen en anderen te helpen bij het overwinnen van dezelfde uitdagingen die wij hebben doorstaan. Zo ontstond
+        </p>
+
+        <h2>Missie</h2>
+        <p>
+            Onze missie is om planners te creëren die niet alleen functioneel zijn, maar ook een verlengstuk zijn van uw persoonlijkheid, doelen en levensstijl.
+        </p>
+
+        <h2>Visie</h2>
+        <p>
+            Onze visie is het omarmen van individualiteit en het koesteren van persoonlijke groei door middel van gepersonaliseerde planners.
+        </p>
     </body>
 </html>
--- Website/producten.php
+++ Website/producten.php
@@ -17,7 +17,30 @@
             </div>
         </nav>
 
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
-</body>
+        <section class="services" id="tips">
+            <div class="our_services">
+                <article>
+                    <div>
+                        <img src="images/card_1.png" alt="" class="prod_img">
+                        <h3>Functionaliteit & Stijl</h3>
+                        <p>Een effectieve planner moet niet alleen praktisch zijn, maar ook een plezier om te gebruiken. Daarom combineren we funtionaliteit met stijvol design.</p>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <img src="images/card_2.png" alt="" class="prod_img">
+                        <h3>Uniekheid</h3>
+                        <p>Wij geloven in de schoonheid van individualiteit. Onze planners worden op maat gemaakt om naadloos aan te sluiten op uw behoeften en voorkeuren.</p>
+                    </div>
+                </article>
+                <article>
+                    <div>
+                        <img src="images/card_3.png" alt="" class="prod_img">
+                        <h3>Inspireren tot Actie</h3>
+                        <p>Een planner is meer dan alleen papier en inkt; het is een instrument voor groei en succes. We streven ernaar om planners te creeren die inspireren tot actie.</p>
+                    </div>
+                </article>
+            </div>
+        </section>
+    </body>
 </html>
