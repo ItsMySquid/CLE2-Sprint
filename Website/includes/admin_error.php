88bb40db552510e5e0b0945b2e0fd341d50bdff2
wesle <wesleynederpel43@gmail.com>
2024-01-18 11:52:52 +0100

    Change directory files

--- Website/includes/admin_error.php
+++ Website/includes/admin_error.php
@@ -1,1 +1,15 @@
 <?php
+
+$errors = [];
+
+if (empty($user)) {
+    $errors['user'] = "De gebruikersnaam mag niet leeg zijn.";
+}
+
+if (empty($password)) {
+    $errors['password'] = "Het wachtwoord mag niet leeg zijn";
+}
+
+if (strlen($password) < 8) {
+    $errors['password'] = 'Het wachtwoord moet langer zijn dan 8 karakters';
+}
