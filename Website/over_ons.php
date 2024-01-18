c029f4b533d38b63d7a173667aef27222e3194a5
D_Zenith <dlw2002@outlook.com>
2024-01-18 13:41:31 +0100

    Over ons pagina vergeten te pushen.

--- Website/over_ons.php
+++ Website/over_ons.php
@@ -17,21 +17,34 @@
             </div>
         </nav>
 
-        <h1>Wie ben ik?</h1>
-        <p>
-            Hallo, ik ben Elif, een 40-jarige moeder van vier prachtige kinderen, waarvan twee te maken hebben met ADHD en dyslexie. Plannen was voor ons geen routine, maar eerder een uitdaging die we moesten overwinnen.
-            Mijn reis begon toen ik mijn eerste planner maakte voor mijn zoon, in een poging om structuur en orde te brengen in zijn dagelijkse leven. Na verschillende iteraties en intensieve tests ontdekte ik dat deze aangepaste planners niet alleen hem, maar ook ons als gezin enorm hielpen.
-            Wat begon als een persoonlijke missie om mijn kinderen te ondersteunen, groeide al snel uit tot een diepere passie. Ik besloot mijn ervaring te delen en anderen te helpen bij het overwinnen van dezelfde uitdagingen die wij hebben doorstaan. Zo ontstond
-        </p>
+        <div class="about_us_bubble">
+            <div class="about_us_text">
+                <h1 class="title_text">Wie ben ik?</h1>
+                <p class="intro_text">
+                    Hallo, ik ben Elif, een 40-jarige moeder van vier prachtige kinderen, waarvan twee te maken hebben met ADHD en dyslexie. Plannen was voor ons geen routine, maar eerder een uitdaging die we moesten overwinnen.
+                    Mijn reis begon toen ik mijn eerste planner maakte voor mijn zoon, in een poging om structuur en orde te brengen in zijn dagelijkse leven. Na verschillende iteraties en intensieve tests ontdekte ik dat deze aangepaste planners niet alleen hem, maar ook ons als gezin enorm hielpen.
+                    Wat begon als een persoonlijke missie om mijn kinderen te ondersteunen, groeide al snel uit tot een diepere passie. Ik besloot mijn ervaring te delen en anderen te helpen bij het overwinnen van dezelfde uitdagingen die wij hebben doorstaan. Zo ontstond
+                </p>
 
-        <h2>Missie</h2>
-        <p>
-            Onze missie is om planners te creëren die niet alleen functioneel zijn, maar ook een verlengstuk zijn van uw persoonlijkheid, doelen en levensstijl.
-        </p>
+                <div class="about_us_mini_bubbles">
+                    <div class="mission_text">
+                        <h2 class="mission_title">Missie</h2>
+                        <p class="mission_info">
+                            Onze missie is om planners te creëren die niet alleen functioneel zijn, maar ook een verlengstuk zijn van uw persoonlijkheid, doelen en levensstijl.
+                        </p>
+                    </div>
 
-        <h2>Visie</h2>
-        <p>
-            Onze visie is het omarmen van individualiteit en het koesteren van persoonlijke groei door middel van gepersonaliseerde planners.
-        </p>
+                    <div class="vision_text">
+                        <h2 class="vision_title">Visie</h2>
+                        <p class="vision_info">
+                            Onze visie is het omarmen van individualiteit en het koesteren van persoonlijke groei door middel van gepersonaliseerde planners.
+                        </p>
+                    </div>
+                </div>
+            </div>
+
+            <img class="about_us_picture" src="images/elif.png" alt="">
+
+        </div>
     </body>
 </html>
